b4db456a6a4b2e8bc1d73abd4db89ee1c5a70a03
chan kam fai <alan.chankf@gmail.com>
2023-12-29 16:33:44 +0000

    updated

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -4,6 +4,7 @@
 #include "../../examples/autotest-validate/autotest-validate.h"
 #include "../../assignment-autotest/test/assignment1/username-from-conf-file.h"
 
+
 /**
 * This function should:
 *   1) Call the my_username() function in autotest-validate.c to get your hard coded username.
@@ -18,5 +19,7 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+
+    TEST_ASSERT_EQUAL_STRING_MESSAGE(my_username(), malloc_username_from_conf_file(), "Username in conf/username.txt is not equal to hard coded username!");
+
 }
